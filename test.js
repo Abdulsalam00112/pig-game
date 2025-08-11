b550f610f1277d2d130bd362db5ab4b70a9e77ee
Abdulsalam00112 <atoyebiabdulsalam324@gmail.com>
2025-08-11 13:38:07 -0700

    new Update

--- test.js
+++ test.js
@@ -33,7 +33,7 @@ scores[activePlayer] += roundscore
 document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
 
 // Check if player won the game 
-if(scores[activePlayer] >= 20){
+if(scores[activePlayer] >= 100){
          document.querySelector('#player-' + activePlayer ).textContent = 'Winner!!!';
          document.querySelector(".dice").style.display = "none";
          document.querySelector(".player-" + activePlayer + "-BG").classList.add("winner")
